46c69af9a4043db793a27574ffea8bbf9f66aaaf
Biittaa <123160799+Biittaa@users.noreply.github.com>
2025-01-27 21:38:00 +0330

    Implement mutation for evolutionary algorithm

--- mutation.py
+++ mutation.py
@@ -1,6 +1,17 @@
 from individual import Individual
-
+import random
 
 def mutation1(individual: Individual):
     # TODO: this method applies mutation on an individual
-    pass
+
+    genome_length = len(individual.genome)
+    
+    position_one = random.randint(0,genome_length-1)
+    position_two = random.randint(0,genome_length-1)
+
+    while position_one == position_two :
+         position_two = random.randint(0,genome_length-1)
+    
+    individual.genome[position_one],individual.genome[position_two] = individual.genome[position_two],individual.genome[position_one]
+    
+    return individual
