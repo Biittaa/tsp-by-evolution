39a068244c451abb63491a12408c328c3cfa95e4
Biittaa <123160799+Biittaa@users.noreply.github.com>
2025-01-27 21:30:31 +0330

    Implement dataset preparation

--- dataset_preparation.py
+++ dataset_preparation.py
@@ -1,3 +1,28 @@
+import math
 def create_distnce_matrix(input_path: str):
     # TODO: read data from file and create distance matrix
-    pass
+
+    with open(input_path,'r') as file:
+        city_positions = [
+            tuple(map(int,line.strip().split(','))) for line in file
+        ]
+    print("city positions:")
+    print(city_positions) 
+    print()
+
+    city_count = len(city_positions) 
+
+    distance_matrix = [[0.0]* city_count for _ in range(city_count)]
+
+    for i in range(city_count):
+        for j in range(city_count):
+            x1 , y1 = city_positions[i]
+            x2 , y2 = city_positions[j]
+            distance_matrix[i][j] =   math.sqrt ((x1-x2)**2 + (y1-y2)**2)
+
+
+    print("Distance Matrix:")
+    print(distance_matrix)        
+
+    return distance_matrix
+
